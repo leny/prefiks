584e9e44a6b1b868994564ae944a06525b5f60a1
Leny <info@flatland.be>
2014-07-11 18:57:09 +0200

    Test browser aliases

--- test/prefiks_test.js
+++ test/prefiks_test.js
@@ -27,24 +27,38 @@ exports[ "prefiks" ] = {
     done();
   },
   "errors": function( test ) {
-    test.throws( function() { prefiks( "css-filters", "nothing", 2 ) }, Error, "should throws for unknown browsers" );
-    test.throws( function() { prefiks( "nothing", "ie", 2 ) }, Error, "should throws for unknown features" );
+    test.throws( function() { prefiks( "css-filters", "nothing", 2 ) }, Error, "Should throws for unknown browsers" );
+    test.throws( function() { prefiks( "nothing", "ie", 2 ) }, Error, "Should throws for unknown features" );
+    test.done();
+  },
+  "browsers aliases": function( test ) {
+    // TODO : there's many more aliases to add & test !
+    // IE aliases
+    test.doesNotThrow( function() { prefiks( "css-filters", "IE" ) }, Error, "Should not throw." );
+    test.doesNotThrow( function() { prefiks( "css-filters", "Internet Explorer" ) }, Error, "Should not throw." );
+    test.doesNotThrow( function() { prefiks( "css-filters", "InternetExplorer" ) }, Error, "Should not throw." );
+    test.doesNotThrow( function() { prefiks( "css-filters", "Internet-Explorer" ) }, Error, "Should not throw." );
+    test.doesNotThrow( function() { prefiks( "css-filters", "INTERNET_EXPLORER" ) }, Error, "Should not throw." );
+    // FF aliases
+    test.doesNotThrow( function() { prefiks( "css-filters", "ff" ) }, Error, "Should not throw." );
+    // IOS Safari Aliases
+    test.doesNotThrow( function() { prefiks( "css-filters", "ios" ) }, Error, "Should not throw." );
     test.done();
   },
   "no version given": function( test ) {
-    test.deepEqual( prefiks( "transforms2d", "ie" ), [ "ms" ], "should be [ 'ms' ]." );
-    test.deepEqual( prefiks( "transforms2d", "firefox" ), [ "moz" ], "should be [ 'moz' ]." );
-    test.deepEqual( prefiks( "transforms2d", "chrome" ), [ "webkit" ], "should be [ 'webkit' ]." );
-    test.deepEqual( prefiks( "transforms2d", "safari" ), [ "webkit" ], "should be [ 'webkit' ]." );
-    test.deepEqual( prefiks( "transforms2d", "opera" ), [ "o", "webkit" ], "should be [ 'o', 'webkit' ]." );
-    test.deepEqual( prefiks( "transforms2d", "ios_saf" ), [ "webkit" ], "should be [ 'webkit' ]." );
-    test.deepEqual( prefiks( "transforms2d", "op_mini" ), [], "should be []." );
-    test.deepEqual( prefiks( "transforms2d", "android" ), [ "webkit" ], "should be [ 'webkit' ]." );
-    test.deepEqual( prefiks( "transforms2d", "bb" ), [ "webkit" ], "should be [ 'webkit' ]." );
-    test.deepEqual( prefiks( "transforms2d", "op_mob" ), [ "webkit" ], "should be [ 'webkit' ]." );
-    test.deepEqual( prefiks( "transforms2d", "and_chr" ), [ "webkit" ], "should be [ 'webkit' ]." );
-    test.deepEqual( prefiks( "transforms2d", "and_ff" ), [], "should be []." );
-    test.deepEqual( prefiks( "transforms2d", "ie_mob" ), [], "should be []." );
+    test.deepEqual( prefiks( "transforms2d", "ie" ), [ "ms" ], "Should be [ 'ms' ]." );
+    test.deepEqual( prefiks( "transforms2d", "firefox" ), [ "moz" ], "Should be [ 'moz' ]." );
+    test.deepEqual( prefiks( "transforms2d", "chrome" ), [ "webkit" ], "Should be [ 'webkit' ]." );
+    test.deepEqual( prefiks( "transforms2d", "safari" ), [ "webkit" ], "Should be [ 'webkit' ]." );
+    test.deepEqual( prefiks( "transforms2d", "opera" ), [ "o", "webkit" ], "Should be [ 'o', 'webkit' ]." );
+    test.deepEqual( prefiks( "transforms2d", "ios_saf" ), [ "webkit" ], "Should be [ 'webkit' ]." );
+    test.deepEqual( prefiks( "transforms2d", "op_mini" ), [], "Should be []." );
+    test.deepEqual( prefiks( "transforms2d", "android" ), [ "webkit" ], "Should be [ 'webkit' ]." );
+    test.deepEqual( prefiks( "transforms2d", "bb" ), [ "webkit" ], "Should be [ 'webkit' ]." );
+    test.deepEqual( prefiks( "transforms2d", "op_mob" ), [ "webkit" ], "Should be [ 'webkit' ]." );
+    test.deepEqual( prefiks( "transforms2d", "and_chr" ), [ "webkit" ], "Should be [ 'webkit' ]." );
+    test.deepEqual( prefiks( "transforms2d", "and_ff" ), [], "Should be []." );
+    test.deepEqual( prefiks( "transforms2d", "ie_mob" ), [], "Should be []." );
     test.done();
   },
 };
